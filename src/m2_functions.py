061cfaaa28aef7123940391df0e2751a90479b0c
bissmepj <bissmepj@rose-hulman.edu>
2017-11-29 09:01:53 -0500

    Partial Completion

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,12 +3,12 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Phil Bissmeyer.
 """
 
 ########################################################################
 #
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE and...
+# DONE: 1. PUT YOUR NAME IN THE ABOVE LINE and...
 #
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
@@ -34,9 +34,11 @@ def main():
     window = rg.TurtleWindow()
 
     turtle1()
+    turtle4()
     turtle3()
     turtle2()
     turtle2()
+    turtle5()
 
     window.close_on_mouse_click()
 
@@ -103,9 +105,35 @@ def turtle3():
     maja.end_fill()
 
 
+def turtle4():
+    phil = rg.SimpleTurtle()
+    phil.pen = rg.Pen('coral',20)
+
+    phil.forward(150)
+    phil.left(90)
+    phil.draw_square(50)
+
+
+def turtle5():
+    nate = rg.SimpleTurtle()
+    carson = rg.SimpleTurtle()
+
+    nate.pen = rg.Pen('DarkRed',20)
+    carson.pen = rg.Pen('DimGrey',10)
+
+    nate.right(180)
+
+    nate.forward(75)
+    carson.forward(75)
+
+    nate.draw_regular_polygon(5,50)
+    carson.draw_regular_polygon(5,50)
+
+
+
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   READ the code above.  Be sure you understand:
 #     -- How many functions are defined above?
 #           (Answer: 4)
@@ -137,7 +165,7 @@ def turtle3():
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Define another function,
 #   immediately below the end of the definition of   turtle3   above.
 #   Name your new function   turtle4.
@@ -167,7 +195,7 @@ def turtle3():
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a line to   main   that CALLS your new function immediately
 #   AFTER  main  calls turtle1.  So:
 #     -- the SimpleTurtle from turtle1 should move,
